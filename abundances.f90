fa21cf8a3e876ee2086972092a4df3089afe3df4
garrelt <garrelt>
2007-10-05 22:59:42 +0000

    Addition of oxygen abundance (for metal cooling in cooling_hm.f90)

--- abundances.f90
+++ abundances.f90
@@ -2,6 +2,7 @@ module abundances
 
   real(kind=8),parameter :: abu_he=0.1
   real(kind=8),parameter :: abu_c=3.3e-4
+  real(kind=8),parameter :: abu_o=3.0e-4
 
   real(kind=8),parameter :: mu=(1.0-abu_he)+4.0*abu_he
 
