ce1891a18bafea71aa26974f993e40d485993e40
garrelt <garrelt>
2010-02-18 10:00:05 +0000

    Synchronization with C2-Ray, doxygen comments

--- atomic.f90
+++ atomic.f90
@@ -1,13 +1,18 @@
+!>
+!! \brief This module contains atomic constants and parameter definitions
+!!
+!! Module for Capreole / C2-Ray (f90)
+!!
+!! \b Author: Garrelt Mellema
+!!
+!! \b Date: 2010-02-04 (2003-06-01)
+!!
+!! \b Version: cgs
+!!
+!! This module is also accepted by the F compiler (Dec 9, 2003)
+!<
+
 module atomic
-
-  ! Module for Capreole (f90)
-  ! Author: Garrelt Mellema
-  ! Date: 2003-06-01
-  ! This module is also accepted by the F compiler (rechecked Dec 24, 2007)
-
-  ! This module contains atomic constants and parameter definitions 
-
-  ! Version: cgs
 
   use precision, only: dp
   use scaling, only: scvelo
@@ -15,9 +20,10 @@ module atomic
 
   private
 
+  !> Boltzmann constant over proton mass (gas constant)
   real(kind=dp),public,parameter :: boltzm = kb/(m_p*scvelo*scvelo)
 
-  ! adiabatic index
+  !> adiabatic index
   real(kind=dp),public,parameter :: gamma = 5.0_dp/3.0_dp
   real(kind=dp),public,parameter :: gamma1 = gamma - 1.0_dp
 
