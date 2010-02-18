bb373edb17d7607260a5bd851e27ddb761d1a452
garrelt <garrelt>
2010-02-18 10:50:37 +0000

    Synching with C2-Ray, doxygen comments

--- abundances.f90
+++ abundances.f90
@@ -1,20 +1,32 @@
+!>
+!! \brief This module sets elemental abundances
+!!
+!! Module for Capreole / C2-Ray (f90)
+!!
+!! \b Author: Garrelt Mellema
+!!
+!! \b Date: 2008-05-27
+!!
+!! This module is also accepted by the F compiler (Dec 9, 2003)
+!<
+
 module abundances
-
-  ! Module for Capreole (f90) or C2-Ray
-  ! Author: Garrelt Mellema
-  ! Date: 2007-12-24 (older but no previous date given)
-  ! This module is also accepted by the F compiler (checked Dec 24, 2007)
-
-  ! This module contains abundance related parameter definitions
 
   use precision, only: dp
 
-  real(kind=dp),parameter,public :: abu_he=0.1_dp
-  real(kind=dp),parameter,public :: abu_c=3.3e-4_dp
+  implicit none
+  
+  private
+  
+  !> Helium abundance (by number) 
+  real(kind=dp),public,parameter :: abu_he=0.1_dp
+  !> Carbon abundance (by number) 
+  real(kind=dp),public,parameter :: abu_c=3.3e-4_dp
+  !> Oxygen abundance (by number) 
   real(kind=dp),parameter,public :: abu_o=3.0e-4_dp
 
-  ! Mean molecular weight (assuming metallicities of order 10^-4).
-  real(kind=dp),parameter,public :: mu=(1.0_dp-abu_he)+4.0_dp*abu_he
+  !> Mean molecular weight (assuming metallicities of order 10^-4). 
+  real(kind=dp),public,parameter :: mu=(1.0-abu_he)+4.0*abu_he
 
   !-----------------------------------------------------------------------
   !     set heavy element abundances
--- sizes.f90
+++ sizes.f90
@@ -1,33 +1,35 @@
+!>
+!! \brief This module contains basic size parameter definitions
+!!
+!! Module for Capreole
+!!
+!! \b Author: Garrelt Mellema
+!!
+!! \b Date: 2010-02-04
+!!
+!! This module is also accepted by the F compiler (Dec 9, 2003)
+
 module sizes
 
-  ! Module for Capreole (f90)
-  ! Author: Garrelt Mellema
-  ! Date: 2003-06-01
-  ! This module is also accepted by the F compiler (Dec 9, 2003)
-
-  ! This module contains basic parameter definitions
-
-  use precision
   private
 
-  integer,public,parameter :: nrOfDim=3        ! number of dimensions
-  integer,public,parameter :: mbc=2            ! number of ghost cells
-  integer,public,parameter :: neuler=2+NrOfDim ! number of Euler equations
-  integer,public,parameter :: neq=neuler+3     ! number of equations (Euler +
-                                               !   advected quantities)
+  integer,public,parameter :: nrOfDim=3        !< number of dimensions
+  integer,public,parameter :: mbc=2            !< number of ghost cells
+  integer,public,parameter :: neuler=2+NrOfDim !< number of Euler equations
+  integer,public,parameter :: neq=neuler+2     !< number of equations (Euler + advected quantities)
   ! Indices of state array
-  integer,public,parameter :: RHO=1
-  integer,public,parameter :: RHVX=2
-  integer,public,parameter :: RHVY=3
-  integer,public,parameter :: RHVZ=4
-  integer,public,parameter :: EN=5
-  integer,public,parameter :: TRACER1=6
-  integer,public,parameter :: XHI=7
-  integer,public,parameter :: XHII=8
+  integer,public,parameter :: RHO=1 !< Indices of state array: density
+  integer,public,parameter :: RHVX=2 !< Indices of state array: x momentum density
+  integer,public,parameter :: RHVY=3 !< Indices of state array: y momentum density
+  integer,public,parameter :: RHVZ=4 !< Indices of state array: z momentum density
+  integer,public,parameter :: EN=5 !< Indices of state array: energy density
+  !integer,public,parameter :: TRACER1=6
+  integer,public,parameter :: XHI=6 !< Indices of state array: neutral hydrogen fraction
+  integer,public,parameter :: XHII=7 !< Indices of state array: ionized hydrogen fraction
 
   ! The following define constants for identifying the coordinate system
-  integer,public,parameter :: CART =1
-  integer,public,parameter :: CYL  =2
-  integer,public,parameter :: SPHER=3
+  integer,public,parameter :: CART =1 !< constant for defining cartesian coordinate system
+  integer,public,parameter :: CYL  =2 !< constant for defining cylindrical coordinate system
+  integer,public,parameter :: SPHER=3 !< constant for defining spherical coordinate system
 
 end module sizes
